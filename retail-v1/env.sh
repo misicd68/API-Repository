60375c56e0af83cfe2bd79c269b6a2566eb37fa8
misicd68 <36548137+misicd68@users.noreply.github.com>
2024-02-08 12:38:45 -0500

    remove

--- retail-v1/env.sh
+++ retail-v1/env.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-# Copyright 2023 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 export PROJECT="apigee-412820"
 export APIGEE_HOST="apigee-412820-eval.apigee.net"
