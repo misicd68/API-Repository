002eb58891887d588d1982fd800aa80aff5cb73c
misicd68 <36548137+misicd68@users.noreply.github.com>
2024-02-08 12:24:05 -0500

    Update env.sh

--- retail-v1/env.sh
+++ retail-v1/env.sh
@@ -15,6 +15,7 @@
 
 export PROJECT="apigee-412820"
 export APIGEE_HOST="apigee-412820-eval.apigee.net"
+export APIGEE_ORG="apigee-412820"
 export APIGEE_ENV="eval"
 PROJECT_NUMBER="$(gcloud projects describe $PROJECT --format="value(projectNumber)")"
 export PROJECT_NUMBER
